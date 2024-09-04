d8809cfb65c6cc159ba96c3df6810118b2919b89
Mathys LYOEN--JARRY <38191389+Maasaki@users.noreply.github.com>
2024-09-04 12:07:45 -1000

    Correction de bugs

--- ASN_pfSense.py
+++ ASN_pfSense.py
@@ -2,15 +2,25 @@ import requests
 import re
 import os
 
-def get_asn_ipv4(asn_numbers, output_file):
-    # Vérifier si le fichier existe déjà
-    if os.path.exists(output_file):
-        os.remove(output_file)
+def get_asn_ip(asn_numbers):
+    # Nom du dossier où stocker les fichiers
+    directory = "ASN_VPN_pfSense"
+    
+    # Vérifier si le dossier existe, sinon le créer
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
-    with open(output_file, 'w') as file:
-        for asn_number in asn_numbers:
-            # Construire l'URL pour chaque ASN
-            url = f"https://www.enjen.net/asn-blocklist/index.php?asn={asn_number.strip()}&type=nginx"
+    for asn_number in asn_numbers:
+        # Générer le nom du fichier pour chaque ASN à l'intérieur du dossier
+        output_file = os.path.join(directory, f"pfsense_vpn_AS{asn_number.strip()}.txt")
+
+        # Vérifier si le fichier existe déjà, si oui, le supprimer
+        if os.path.exists(output_file):
+            os.remove(output_file)
+
+        with open(output_file, 'w') as file:
+            # Construire l'URL pour chaque ASN avec `type=iplist`
+            url = f"https://www.enjen.net/asn-blocklist/index.php?asn={asn_number.strip()}&type=iplist"
             
             # Effectuer la requête GET
             response = requests.get(url)
@@ -20,27 +30,37 @@ def get_asn_ipv4(asn_numbers, output_file):
                 # Récupérer le contenu de la réponse
                 cleaned_data = re.sub(r'<br\s*/?>', '\n', response.text)
                 
-                # Utiliser une regex pour extraire uniquement les IPv4
+                # Utiliser une regex pour extraire les IPv4 et IPv6
                 ipv4_regex = r"(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/\d{1,2})"
-                ipv4_addresses = re.findall(ipv4_regex, cleaned_data)
+                ipv6_regex = r"([a-fA-F0-9:]+:+[a-fA-F0-9:]+/\d{1,3})"
                 
+                ipv4_addresses = re.findall(ipv4_regex, cleaned_data)
+                ipv6_addresses = re.findall(ipv6_regex, cleaned_data)
+                
+                # Ajouter un commentaire pour chaque ASN
+                file.write(f"# ASN Number : {asn_number.strip()}\n")
+                
+                # Écrire les plages IPv4 dans le fichier
                 if ipv4_addresses:
-                    # Ajouter un commentaire pour chaque ASN
-                    file.write(f"# ASN Number : {asn_number.strip()}\n")
-                    # Écrire les plages IPv4 dans le fichier en ajoutant un point-virgule
+                    file.write("# IPv4 addresses:\n")
                     file.write(" ;\n".join(ipv4_addresses) + " ;\n\n")
                 else:
-                    print(f"Aucune adresse IPv4 trouvée pour l'ASN {asn_number.strip()}.")
+                    file.write("# No IPv4 addresses found.\n\n")
+                
+                # Écrire les plages IPv6 dans le fichier
+                if ipv6_addresses:
+                    file.write("# IPv6 addresses:\n")
+                    file.write(" ;\n".join(ipv6_addresses) + " ;\n\n")
+                else:
+                    file.write("# No IPv6 addresses found.\n\n")
+                    
             else:
                 print(f"Erreur lors de la récupération des données pour l'ASN {asn_number.strip()}. Code de statut: {response.status_code}")
 
-    print(f"Les plages IPv4 pour les ASN {', '.join(asn_numbers)} ont été enregistrées dans {output_file}.")
+        print(f"Les plages IPv4 et IPv6 pour l'ASN {asn_number.strip()} ont été enregistrées dans {output_file}.")
 
 # Ajouter manuellement les numéros ASN ici
 asn_numbers = ["63023", "13335", "21859", "62240", "14061", "40021", "136787", "40676", "61272"]
 
-# Spécifier le nom du fichier de sortie
-output_file = "pfsense_vpn.txt"
-
 # Appeler la fonction avec les ASN fournis
-get_asn_ipv4(asn_numbers, output_file)
+get_asn_ip(asn_numbers)
