1a02604ecff08c14ba649e07bdb93e227182fa81
Christophe Raffalli <christophe@raffalli.eu>
2024-09-05 15:27:29 -1000

    add all ips in one file

--- ASN_pfSense2.py
+++ ASN_pfSense2.py
@@ -14,44 +14,59 @@ def get_asn_ip(asn_numbers):
     if not os.path.exists(directory):
         os.makedirs(directory)
 
-    for asn_number in asn_numbers:
-        # Générer le nom du fichier pour chaque ASN à l'intérieur du dossier
-        output_file = os.path.join(directory, f"pfsense_vpn_AS{asn_number.strip()}.txt")
+    # Générer le nom du fichier pour chaque ASN à l'intérieur du dossier
+    output_file = os.path.join(directory, f"pfsense_vpn_ALL.txt")
 
-        # Vérifier si le fichier existe déjà, si oui, le supprimer
-        if os.path.exists(output_file):
-            os.remove(output_file)
+    # Vérifier si le fichier existe déjà, si oui, le supprimer
+    if os.path.exists(output_file):
+        os.remove(output_file)
 
-        with open(output_file, 'w') as file:
-            # Construire l'URL pour chaque ASN avec `type=iplist`
-            url = f"https://api.hackertarget.com/aslookup/?q={asn_number.strip()}&output=json"
+    with open(output_file, 'w') as all:
 
-            # Effectuer la requête GET
-            response = requests.get(url)
+        for asn_number in asn_numbers:
+            output_file = os.path.join(directory, f"pfsense_vpn_AS{asn_number.strip()}.txt")
 
-            # Vérifier que la requête s'est bien passée
-            if response.status_code == 200:
-                # Récupérer le contenu de la réponse
-                cleaned_data = json.loads(response.text)
-                asn_num = cleaned_data['as_number']
-                ips = cleaned_data['prefixes']
+            # Vérifier si le fichier existe déjà, si oui, le supprimer
+            if os.path.exists(output_file):
+                os.remove(output_file)
 
-                file.write(f"# ASN Number : {asn_num}\n")
+            with open(output_file, 'w') as file:
+                # Construire l'URL pour chaque ASN avec `type=iplist`
+                url = f"https://api.hackertarget.com/aslookup/?q={asn_number.strip()}&output=json"
 
-                file.write("# IPv4 addresses:\n")
-                for ip in ips:
-                    if re.match(ipv4_regex, ip):
-                        file.write(f"{ip} ;\n")
+                # Effectuer la requête GET
+                response = requests.get(url)
 
-                file.write("# IPv6 addresses:\n")
-                for ip in ips:
-                    if re.match(ipv6_regex, ip):
-                        file.write(f"{ip} ;\n")
+                # Vérifier que la requête s'est bien passée
+                if response.status_code == 200:
+                    # Récupérer le contenu de la réponse
+                    cleaned_data = json.loads(response.text)
+                    asn_num = cleaned_data['as_number']
+                    ips = cleaned_data['prefixes']
 
-            else:
-                print(f"Erreur lors de la récupération des données pour l'ASN {asn_number.strip()}. Code de statut: {response.status_code}")
+                    file.write(f"# ASN Number : {asn_num}\n")
+                    all.write(f"# ASN Number : {asn_num}\n")
 
-        print(f"Les plages IPv4 et IPv6 pour l'ASN {asn_number.strip()} ont été enregistrées dans {output_file}.")
+                    file.write("# IPv4 addresses:\n")
+                    all.write("# IPv4 addresses:\n")
+
+                    for ip in ips:
+                        if re.match(ipv4_regex, ip):
+                            file.write(f"{ip} ;\n")
+                            all.write(f"{ip} ;\n")
+
+                    file.write("# IPv6 addresses:\n")
+                    all.write("# IPv6 addresses:\n")
+
+                    for ip in ips:
+                        if re.match(ipv6_regex, ip):
+                            file.write(f"{ip} ;\n")
+                            all.write(f"{ip} ;\n")
+
+                else:
+                    print(f"Erreur lors de la récupération des données pour l'ASN {asn_number.strip()}. Code de statut: {response.status_code}")
+
+            print(f"Les plages IPv4 et IPv6 pour l'ASN {asn_number.strip()} ont été enregistrées dans {output_file}.")
 
 # Ajouter manuellement les numéros ASN ici
 asn_numbers = ["63023", "21859", "62240", "14061", "40021", "136787", "40676", "61272"]
